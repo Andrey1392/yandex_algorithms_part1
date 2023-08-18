7bcfa7fd7dd6d2fc76c25e609f543170e9232715
Андрей Поваляев <povalyaev.andrey@gmail.com>
2023-08-19 00:15:34 +0500

    corrections after 2nd review

--- find_distances_to_zero.py
+++ find_distances_to_zero.py
@@ -1,99 +1,44 @@
-# номер посылки 89659519
-
-def calculate_distances_two_boundaries(array_length):
-    '''
-    Расчет минимальных расстояний до пустого дома
-    для линии, имеющей пустые дома слева и справа
-    '''
-    rigth_half_distances = calculate_distances_left_boundary(array_length // 2)
-    center_distance = [array_length // 2 + 1 for _ in range(array_length % 2)]
-    left_half_distances = rigth_half_distances[::-1]
-    return left_half_distances + center_distance + rigth_half_distances
+# номер посылки 89718101
 
 
-def calculate_distances_left_boundary(array_length):
-    '''
-    Расчет минимальных расстояний до пустого дома
-    для линии, не имеющех пустых домов слева
-    '''
-    return [array_length - i for i in range(array_length)]
+def distances_to_nearest_zero(numbers):
 
+    street_length = len(numbers)
+    distances = [0] * street_length
 
-def calculate_distances_to_empty_house(street_length, numbers_list):
-    '''
-    Расчет минимальных расстояний до пустого дома
-    нулевая линия - массив, состоящий из номеров пустых домов
-    ненулевая линия - массив, состоящий из номеров не пустых домов
-    '''
+    zeroes_indexes = [i for i in range(street_length) if numbers[i] == '0']
+    first_zero_index = zeroes_indexes[0]
+    last_zero_index = zeroes_indexes[-1]
 
-    # итоговый массив
-    distances_to_empty_house = [0] * street_length
+    distances[:first_zero_index] = [
+        first_zero_index - i for i in range(first_zero_index)
+    ]
 
-    # левая и правая граница ненулевой линии
-    left_boundary_index = 0
-    right_boundary_index = 0
+    for i in range(len(zeroes_indexes) - 1):
+        left_boundary_index = zeroes_indexes[i]
+        right_boundary_index = zeroes_indexes[i+1]
+        interval_length = right_boundary_index - left_boundary_index - 1
+        if interval_length > 0:
+            left_half_distances = [
+                i + 1 for i in range(interval_length // 2)
+            ]
+            center_distance = [
+                interval_length // 2 + 1 for _ in range(interval_length % 2)
+            ]
+            rigth_half_distances = left_half_distances[::-1]
+            distances[
+                left_boundary_index + 1: right_boundary_index
+            ] = left_half_distances + center_distance + rigth_half_distances
 
-    # указатель прохождения цикла по нулевой линии
-    empty_houses_line_flag = True
-    for i in range(street_length):
-        house_num = numbers_list[i]
-        if house_num == '0':
-            if empty_houses_line_flag:
-                # проходим дальше по нулевой линии,
-                # ставим краевые индексы на текущую позицию в массиве
-                left_boundary_index, right_boundary_index = i, i
-            else:
-                # закончилась ненулевая линия,
-                # рассчитываем расстояния для линии
-                if left_boundary_index == 0:
-                    distances_in_line = calculate_distances_left_boundary(
-                        right_boundary_index - left_boundary_index + 1
-                    )
-                else:
-                    distances_in_line = calculate_distances_two_boundaries(
-                        right_boundary_index - left_boundary_index + 1
-                    )
+    distances[last_zero_index+1:] = [
+        i + 1 for i in range(street_length - (last_zero_index + 1))
+    ]
 
-                # вставляем полученные расстояния в результирующий массив
-                distances_to_empty_house[
-                    left_boundary_index:right_boundary_index + 1
-                ] = distances_in_line
-
-                # ставим краевые индексы на текущую позицию в массиве
-                left_boundary_index, right_boundary_index = i, i
-                empty_houses_line_flag = True
-        else:
-            # если находимся в начале ненулевой линии,
-            # ставим краевые индексы на текущую позицию в массиве
-            if empty_houses_line_flag:
-                left_boundary_index, right_boundary_index = i, i
-                empty_houses_line_flag = False
-
-            else:
-                # идем по ненулевой линии
-                # смещаем правый краевой индекс на 1 вправо
-                right_boundary_index += 1
-
-            # если дошли до конца массива по ненулевой линии,
-            # вычисляем значения расстояний и вставляем в итоговый массив
-            if right_boundary_index == street_length - 1:
-                distances_in_line = calculate_distances_left_boundary(
-                    right_boundary_index - left_boundary_index + 1
-                )[::-1]
-
-                distances_to_empty_house[
-                    left_boundary_index:right_boundary_index + 1
-                ] = distances_in_line
-
-    return distances_to_empty_house
-
-
-def main():
-    street_length = int(input())
-    numbers_list = input().split()
-    result = calculate_distances_to_empty_house(street_length, numbers_list)
-    print(*result)
+    return (distances)
 
 
 if __name__ == '__main__':
-    main()
+    input()
+    numbers = input().split()
+    result = distances_to_nearest_zero(numbers)
+    print(*result)
--- typing_game.py
+++ typing_game.py
@@ -1,27 +1,14 @@
-# номер посылки 89657307
+# номер посылки 89717458
 
-def calculate_score(k, data):
-    GAME_WIN_SCORE = 1
+def calculate_score(count_max, data):
 
-    test_values = [str(v) for v in range(1, 10)]
-
-    round_scores = []
-    for v in test_values:
-        data_length_old = len(data)
-        data = data.replace(v, '')
-        data_length_new = len(data)
-        if k >= data_length_old - data_length_new > 0:
-            round_scores.append(GAME_WIN_SCORE)
-        data_length_old = data_length_new
-    return sum(round_scores)
-
-
-def main():
-    k = 2 * int(input())
-    data = f'{input()}{input()}{input()}{input()}'
-
-    print(calculate_score(k, data))
+    return sum(
+        [1 if count_max >= data.count(str(i)) > 0 else 0 for i in range(1, 10)]
+    )
 
 
 if __name__ == '__main__':
-    main()
+    count_max = 2 * int(input())
+    data = f'{input()}{input()}{input()}{input()}'
+
+    print(calculate_score(count_max, data))
