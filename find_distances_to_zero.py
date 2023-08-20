4a3bfe016b3fcb4baa4c4f42ca38c6dbfb834b67
Андрей Поваляев <povalyaev.andrey@gmail.com>
2023-08-20 15:55:15 +0500

    correections after 4th review

--- find_distances_to_zero.py
+++ find_distances_to_zero.py
@@ -1,26 +1,24 @@
-# номер посылки 89721121
+# номер посылки 89738351
 
 
 def distances_to_nearest_zero(numbers, zero_value='0'):
-
     street_length = len(numbers)
     distances = [0] * street_length
     zeroes = [
-        number_index for number_index, value in enumerate(numbers) if value == zero_value
+        pos for pos, value in enumerate(numbers) if value == zero_value
     ]
     first, last = zeroes[0], zeroes[-1]
     distances[:first] = [
-        first - number_index for number_index in range(first)
+        first - pos for pos in range(first)
     ]
-    for left_index, right_index in zip(zeroes, zeroes[1:]):
-        for number_index in range(left_index + 1, right_index):
-            distances[number_index] = min(
-                number_index - left_index, right_index - number_index
-            )
-    distances[last + 1:] = [
-        number_index - last for number_index in range(last + 1, street_length)
-    ]
+    for left, right in zip(zeroes, zeroes[1:]):
+        distances[left + 1:right] = [
+            min(pos - left, right - pos) for pos in range(left + 1, right)
+        ]
 
+    distances[last + 1:] = [
+        pos - last for pos in range(last + 1, street_length)
+    ]
     return distances
 
 
--- typing_game.py
+++ typing_game.py
@@ -1,20 +1,19 @@
-# номер посылки 89721205
+# номер посылки 89738372
 
 def calculate_score(
         count_max,
         data,
-        values=('1', '2', '3', '4', '5', '6', '7', '8', '9',),
+        values='123456789',
         players=2
 ):
 
     return sum(
-        [count_max * players >= data.count(value) > 0 for value in values]
+        count_max * players + 1 > data.count(value) > 0 for value in values
     )
 
 
 if __name__ == '__main__':
-    print(
-        calculate_score(count_max=int(input()),
-                        data=f'{input()}{input()}{input()}{input()}'
-                        )
-        )
+    print(calculate_score(
+      count_max=int(input()),
+      data=f'{input()}{input()}{input()}{input()}',
+    ))
